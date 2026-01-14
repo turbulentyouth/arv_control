39a72717abad1d50005a23a01fc1fdfa16bf3aa9
xijingfeng <2580577725@qq.com>
2026-01-14 13:10:33 +0800

    使窗口默认最大化

--- src/main.rs
+++ src/main.rs
@@ -3,5 +3,11 @@ slint::include_modules!();
 fn main() -> Result<(), slint::PlatformError> {
     let ui = AppWindow::new()?;
 
+    let weak_ui = ui.as_weak();
+    slint::invoke_from_event_loop(move || {
+        let ui = weak_ui.unwrap();
+        ui.window().set_maximized(true);
+    }).unwrap();
+
     ui.run()
 }
